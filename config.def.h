b66f1a7125dc8cb01bbf1f9ca8629247bc2f1ed3
MGC <chagel@gmail.com>
2019-02-15 18:46:12 -0800

    custimzed font

--- config.def.h
+++ config.def.h
@@ -6,7 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Hack:style=bold;size=20:antialias=true:autohint=true";
-static char *font = "mononoki:size=8:style=Regular:antialias=true:autohint:true";
+//static char *font = "mononoki:size=8:style=Regular:antialias=true:autohint:true";
+static char *font = "Monaco:size=6:style=Regular:antialias=true:autohint:true";
 static int borderpx = 10;
 
 /*
